6350f389da5f2036c95de7cb8be62ac2ccd381e5
jacpimbi <jacksonsylas59@gmail.com>
2022-09-22 10:41:29 +0330

    converted several components(eg: header,cart-item,cart-icon,collection-item and checkout into styled component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
@@ -11,30 +10,30 @@ import { selectCurrentUser  } from '../../redux/user/user.selectors';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-import './header.styles.scss';
+import { HeaderContainer, LogoContainer, OptionContainer, OptionLink } from './header.styles';
 
 const Header = ({ currentUser, hidden }) => (
-     <div className='header'>
-         <Link className='logo-container' to="/">
+     <HeaderContainer>
+         <LogoContainer to="/">
                <Logo className='logo' />
-         </Link>
-         <div className='options'>
-            <Link className='option' to='/shop'>
+         </LogoContainer>
+         <OptionContainer>
+            <OptionLink to='/shop'>
                 SHOP
-            </Link>
-            <Link className='option' to='/contact'>
+            </OptionLink >
+            <OptionLink  to='/contact'>
                 CONTACT 
-            </Link>
+            </OptionLink >
             {
                 currentUser ?(
-                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> )
-                : (<Link className='option' to='/signin'>SIGN IN </Link>)
+                <OptionLink as='div' onClick={() => auth.signOut()}>SIGN OUT</OptionLink> )
+                : (<OptionLink to='/signin'>SIGN IN </OptionLink>)
             } 
             <CartIcon /> 
-         </div>
+         </OptionContainer>
          { hidden ? null :<CartDropDown /> 
          }
-     </div>
+     </HeaderContainer>
 )
 
 const mapStateToProps = createStructuredSelector ({
